d4fc0b0166af6a1c5de8ef0f6672dbd393640175
Ryan Brooks <ryan_brooks@Ryans-MacBook-Pro-3.local>
2017-12-05 09:09:30 -0500

    Added pitch and roll, yaw to be added

--- app/src/main/java/com/ryan_brooks/quadcontroller/BluetoothInterface.kt
+++ app/src/main/java/com/ryan_brooks/quadcontroller/BluetoothInterface.kt
@@ -2,43 +2,306 @@ package com.ryan_brooks.quadcontroller
 
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothSocket
+import android.content.ContentValues
+import android.content.ContentValues.TAG
+import android.os.Bundle
+import android.os.Handler
+import android.os.Message
+import android.util.Log
+import java.io.IOException
+import java.io.InputStream
+import java.io.OutputStream
+import java.util.*
 
-class BluetoothInterface {
+
+class BluetoothInterface(private val handler: Handler) {
 
     companion object {
+        val MESSAGE_CONNECTED = 0
+        val MESSAGE_READ = 1
+        val MESSAGE_WRITE = 2
+        val MESSAGE_TOAST = 3
+
+        val STATE_NONE = 0
+        val STATE_CONNECTING = 1
+        val STATE_CONNECTED = 2
+
+        val HC06_NAME = "quadReceiver"
         val HC06_MAC_ADDRESS = "00:14:03:06:02:47"
+
+        // INSECURE "8ce255c0-200a-11e0-ac64-0800200c9a66"
+        // SECURE "fa87c0d0-afac-11de-8a39-0800200c9a66"
+        // SPP "0001101-0000-1000-8000-00805F9B34FB"
+        val HC06_UUID: UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
     }
 
+    private var state = STATE_NONE
     private val bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
     private var droneDevice: BluetoothDevice? = null
+
+    private var connectThread: ConnectThread? = null
+    private var connectedThread: ConnectedThread? = null
+
 
     init {
         getDroneDevice()
     }
 
-    fun connect() {
-
-    }
-
-    fun disconnect() {
-
-    }
-
     private fun getDroneDevice() {
         val pairedDevices: Set<BluetoothDevice> = bluetoothAdapter.bondedDevices
         for (device in pairedDevices) {
-            if (device.address.toLowerCase() == HC06_MAC_ADDRESS) {
+            Log.w("Device:", device.name)
+            Log.w("Type", device.type.toString())
+            if (device.name == HC06_NAME) {
                 droneDevice = device
+                Log.w("Found device", device.name)
                 continue
             }
         }
     }
 
-    internal inner class ConnectThread: Thread() {
-        // TODO
+    @Synchronized
+    fun connect() {
+        if (droneDevice == null) {
+            // TODO: Report to activity that it's null
+            return
+        }
+
+        // Cancel any thread attempting to make a connection
+        if (state == STATE_CONNECTING) {
+            if (connectThread != null) {
+                connectThread!!.cancel()
+                connectThread = null
+            }
+        }
+
+        // Cancel any thread currently running a connection
+        if (connectedThread != null) {
+            connectedThread!!.cancel()
+            connectedThread = null
+        }
+
+        // Start the thread to connect with the given device
+        connectThread = ConnectThread()
+        connectThread!!.start()
+
+        state = STATE_CONNECTING
     }
 
-    internal inner class DataTransmitThread: Thread() {
+    @Synchronized
+    fun disconnect() {
+        if (connectThread != null) {
+            connectThread!!.cancel()
+            connectThread = null
+        }
 
+        if (connectedThread != null) {
+            connectedThread!!.cancel()
+            connectedThread = null
+        }
+
+        state = STATE_NONE
+    }
+
+    fun sendMessage(message: String) {
+        if (state != STATE_CONNECTED) {
+            Log.w(TAG, "bluetooth is not connected")
+            return
+        }
+
+        if (message.isNotEmpty()) {
+            // val EOT = 3.toChar()
+            // Get the message bytes and tell the BluetoothChatService to write
+            val send = message.toByteArray()
+            write(send)
+        }
+    }
+
+    fun sendFlightInfo(throttle: Byte, yaw: Byte, pitch: Byte, roll: Byte) {
+        if (state != STATE_CONNECTED) {
+            Log.w(TAG, "bluetooth is not connected")
+            return
+        }
+
+        val bytes = ByteArray(4)
+
+        bytes[0] = throttle
+        bytes[1] = yaw
+        bytes[2] = pitch
+        bytes[3] = roll
+        write(bytes)
+    }
+
+    fun isConnected(): Boolean {
+        return state == STATE_CONNECTED
+    }
+
+    private fun sendMessageToHandler(message: Message, bundle: Bundle) {
+        message.data = bundle
+        handler.sendMessage(message)
+    }
+
+    private fun connectionEstablished() {
+        state = STATE_CONNECTED
+
+        val bundle = Bundle()
+        bundle.putString("connected", droneDevice!!.name)
+        sendMessageToHandler(handler.obtainMessage(MESSAGE_CONNECTED), bundle)
+    }
+
+    private fun connectionFailed() {
+        val bundle = Bundle()
+        bundle.putString("toast", "Unable to connect device")
+        sendMessageToHandler(handler.obtainMessage(MESSAGE_TOAST), bundle)
+    }
+
+    private fun connectionLost() {
+        val bundle = Bundle()
+        bundle.putString("toast", "Connection lost")
+        sendMessageToHandler(handler.obtainMessage(MESSAGE_TOAST), bundle)
+    }
+
+    private fun write(out: ByteArray) {
+        // Create temporary object
+        var tempThread: ConnectedThread? = null
+
+        // Synchronize a copy of the ConnectedThread
+        synchronized(this) {
+            if (state != STATE_CONNECTED) {
+                return
+            }
+            tempThread = connectedThread
+        }
+
+        // Perform the write unsynchronized
+        tempThread?.write(out)
+    }
+
+    internal inner class ConnectThread : Thread() {
+
+        private var clientSocket: BluetoothSocket? = null
+
+        init {
+            var socket: BluetoothSocket? = null
+
+            try {
+                socket = droneDevice?.createRfcommSocketToServiceRecord(HC06_UUID)
+            } catch (e: IOException) {
+                Log.e(ContentValues.TAG, "Client Socket's create() method failed", e)
+            }
+
+            clientSocket = socket
+        }
+
+        override fun run() {
+            bluetoothAdapter.cancelDiscovery()
+
+            try {
+                clientSocket!!.connect()
+                Log.d(ContentValues.TAG, "Client socket connected")
+            } catch (connectException: IOException) {
+                try {
+                    Log.e(ContentValues.TAG, "Client socket closed in run()", connectException)
+
+                    clientSocket = droneDevice!!::class.java.getMethod("createRfcommSocket", Int::class.javaPrimitiveType).invoke(droneDevice, 1) as BluetoothSocket?
+                    clientSocket?.connect()
+                } catch (e: Exception) {
+                    connectionFailed()
+                    Log.e(ContentValues.TAG, "Could not close the client socket", e)
+                }
+            }
+
+            connected(clientSocket!!)
+        }
+
+        internal fun cancel() {
+            try {
+                clientSocket!!.close()
+                Log.d(ContentValues.TAG, "Client socket closed")
+            } catch (e: IOException) {
+                Log.e(ContentValues.TAG, "Could not close the client socket", e)
+            }
+
+        }
+    }
+
+    @Synchronized
+    private fun connected(socket: BluetoothSocket) {
+
+        // Cancel any thread currently running a connection
+        if (connectedThread != null) {
+            connectedThread!!.cancel()
+            connectedThread = null
+        }
+
+        // Start the thread to manage the connection and perform transmissions
+        connectedThread = ConnectedThread(socket)
+        connectedThread!!.start()
+
+        connectionEstablished()
+    }
+
+    internal inner class ConnectedThread(private val socket: BluetoothSocket) : Thread() {
+        private var inputStream: InputStream? = null
+        private var outputStream: OutputStream? = null
+
+        init {
+            var tempInputStream: InputStream? = null
+            var tempOutputStream: OutputStream? = null
+
+            try {
+                tempInputStream = socket.inputStream
+                tempOutputStream = socket.outputStream
+            } catch (e: IOException) {
+                Log.e(TAG, "temp sockets not created", e)
+            }
+
+            inputStream = tempInputStream
+            outputStream = tempOutputStream
+        }
+
+        override fun run() {
+            val buffer = ByteArray(1024)
+            var bytes: Int
+
+            // Keep listening to the InputStream while connected
+            while (true) {
+                try {
+                    // Read from the InputStream
+                    bytes = inputStream?.read(buffer)!!
+                    Log.d(TAG, "message bytes " + bytes)
+                    Log.d(TAG, "message string bytes " + bytes.toString())
+                    Log.d(TAG, "message buffer " + String(buffer))
+                    // Send the obtained bytes to the UI Activity
+                    handler.obtainMessage(MESSAGE_READ, bytes, -1, buffer).sendToTarget()
+                } catch (e: IOException) {
+                    Log.e(TAG, "disconnected", e)
+                    connectionLost()
+                    break
+                }
+
+            }
+        }
+
+        fun write(buffer: ByteArray) {
+            try {
+                outputStream?.write(buffer)
+
+                // Share the sent message back to the UI Activity
+                handler.obtainMessage(MESSAGE_WRITE, -1, -1, buffer).sendToTarget()
+            } catch (e: IOException) {
+                Log.e(TAG, "Exception during write", e)
+            }
+
+        }
+
+        fun cancel() {
+            try {
+                socket.close()
+            } catch (e: IOException) {
+                Log.e(TAG, "close() of connect socket failed", e)
+            }
+        }
     }
 }
--- app/src/main/java/com/ryan_brooks/quadcontroller/MainActivity.kt
+++ app/src/main/java/com/ryan_brooks/quadcontroller/MainActivity.kt
@@ -1,19 +1,171 @@
 package com.ryan_brooks.quadcontroller
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Message
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import io.github.controlwear.virtual.joystick.android.JoystickView
+import org.jetbrains.anko.find
+
 
 class MainActivity : AppCompatActivity() {
 
-    val bluetoothInterface = BluetoothInterface()
+    companion object {
+        val BT_AUTO_TRANSMIT_DELAY = 1000L
+
+        val MAX_THROTTLE_STRENGTH = 60
+        val ZERO_THROTTLE_STRENGTH = 100
+    }
+
+    private val bluetoothHandler = BluetoothHandler()
+    private val bluetoothInterface = BluetoothInterface(bluetoothHandler)
+
+    private lateinit var edittext: EditText
+    private lateinit var button: Button
+    private lateinit var connectButton: Button
+    private lateinit var throttleStick: JoystickView
+    private lateinit var aileronStick: JoystickView
+
+    private val dataHandler = Handler()
+
+    private var throttle: Byte = 0
+    private var yaw: Byte = 127
+    private var pitch: Byte = 127
+    private var roll: Byte = 127
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        edittext = find(R.id.message_edittext)
+        button = find(R.id.send_button)
+        connectButton = find(R.id.connect_button)
+        throttleStick = find(R.id.throttle_stick)
+        aileronStick = find(R.id.aileron_stick)
+
+
+        throttleStick.setOnMoveListener(throttleMoveListener)
+        aileronStick.setOnMoveListener(aileronMoveListener)
+
+        button.setOnClickListener({
+            bluetoothInterface.sendMessage(edittext.text.toString().trim())
+        })
+
+        connectButton.setOnClickListener({
+            bluetoothInterface.connect()
+        })
+
+        dataHandler.postDelayed(btTransmitRunnable, BT_AUTO_TRANSMIT_DELAY)
     }
 
     override fun onDestroy() {
         super.onDestroy()
         bluetoothInterface.disconnect()
+        dataHandler.removeCallbacks(btTransmitRunnable)
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun calculateThrottleStrength(angle: Int, strength: Int) {
+        var correctedStrength = strength
+        // Max top throttle = 75
+        if (angle == 90 && strength > MAX_THROTTLE_STRENGTH) {
+            correctedStrength = MAX_THROTTLE_STRENGTH
+        }
+
+        // Max bottom throttle = 0
+        if (angle == 270 && strength > ZERO_THROTTLE_STRENGTH) {
+            correctedStrength = ZERO_THROTTLE_STRENGTH
+        }
+
+        // TODO: Adjust for angles when Yaw added
+        val strengthDifference = 100 - correctedStrength
+        when (angle) {
+            0 -> throttle = 127
+            270 -> {
+                // % of 127
+                throttle = ((strengthDifference.toFloat() / 100) * 127).toByte()
+            }
+            90 -> {
+                // % of 255
+                throttle = ((strengthDifference.toFloat() / 100) * -127).toByte()
+            }
+        }
+    }
+
+    private fun calculatePitchStrength(angle: Int, strength: Int) {
+        var x = ((((127 * Math.sin(Math.toRadians(angle.toDouble())))) * (strength.toDouble() / 100)) * -1).toByte()
+
+        if (strength == 0) {
+            pitch = 127
+            return
+        }
+
+        if (x == 0.toByte()) {
+            x = 127
+        }
+
+        pitch = if (angle in 181..359) {
+            (Math.ceil(127 - x.toDouble())).toByte()
+        } else {
+            x
+        }
+    }
+
+    private fun calculateRollStrength(angle: Int, strength: Int) {
+        var x = ((((127 * Math.cos(Math.toRadians(angle.toDouble())))) * (strength.toDouble() / 100)) * -1).toByte()
+
+        if (strength == 0) {
+            roll = 127
+            return
+        }
+
+        if (x == 0.toByte()) {
+            x = 127
+        }
+
+        roll = if (angle in 91..269) {
+            (Math.ceil(127 - x.toDouble())).toByte()
+        } else {
+            x
+        }
+    }
+
+    private val throttleMoveListener: JoystickView.OnMoveListener = JoystickView.OnMoveListener { angle, strength ->
+        calculateThrottleStrength(angle, strength)
+        bluetoothInterface.sendFlightInfo(throttle, yaw, pitch, roll)
+        // TODO: Yaw
+    }
+
+    private val aileronMoveListener: JoystickView.OnMoveListener = JoystickView.OnMoveListener { angle, strength ->
+        calculateRollStrength(angle, strength)
+        calculatePitchStrength(angle, strength)
+        bluetoothInterface.sendFlightInfo(throttle, yaw, pitch, roll)
+    }
+
+
+    // Send value every X seconds to ensure connection isn't killed
+    private val btTransmitRunnable = object : Runnable {
+        override fun run() {
+            if (bluetoothInterface.isConnected()) {
+                bluetoothInterface.sendFlightInfo(throttle, yaw, pitch, roll)
+            }
+            dataHandler.postDelayed(this, BT_AUTO_TRANSMIT_DELAY)
+        }
+    }
+
+    internal inner class BluetoothHandler : Handler() {
+        override fun handleMessage(msg: Message) {
+            when (msg.what) {
+//                BluetoothInterface.Companion.MESSAGE_TOAST -> showToast(msg.data.getString("connected"))
+                BluetoothInterface.Companion.MESSAGE_TOAST -> showToast(msg.data.getString("toast"))
+            }
+        }
     }
 }
